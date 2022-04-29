4fe8b753ad050aa67eb3c7b376fa850e821c99cb
phillipgoellner <admin@pgoellner.com>
2022-04-30 00:28:54 +0200

    Include a PyPi compatible project description

--- setup.py
+++ setup.py
@@ -2,10 +2,15 @@ from setuptools import setup
 
 import unittest_specs
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="unittest-specs",
     version=unittest_specs.__version__,
     description="A simple wrapper around the unittest package with different styles for specs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/phillipgoellner/unittest_specs",
     author=unittest_specs.__author__,
     license="GPL-3.0 License",
--- unittest_specs/__init__.py
+++ unittest_specs/__init__.py
@@ -4,5 +4,5 @@ unittest-specs
 A simple wrapper around the unittest package with different styles for specsA simple flat spec wrapper around the unittest package
 """
 
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 __author__ = "Phillip Goellner"
