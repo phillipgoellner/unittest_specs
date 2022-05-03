cfeb1920cb66c61538a6b75d8b860cf74b255714
phillipgoellner <admin@pgoellner.com>
2022-05-03 23:57:56 +0200

    Simplify the import for SimpleFlatSpec and increase the version number for a release

--- unittest_specs/__init__.py
+++ unittest_specs/__init__.py
@@ -1,8 +1,11 @@
 """
 unittest-specs
 
-A simple wrapper around the unittest package with different styles for specsA simple flat spec wrapper around the unittest package
+A simple wrapper around the unittest package with different styles for specs
 """
 
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 __author__ = "Phillip Goellner"
+
+
+from unittest_specs.simple_test_spec import SimpleFlatSpec
